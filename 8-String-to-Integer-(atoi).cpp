962dc1a18895835b02604fa41722a95498d99427
Ahmed Ashraf <121035001+AhmedIAshraf@users.noreply.github.com>
2024-06-22 01:59:05 +0300

    [8] [Time Beats: 100.00%] [Memory Beats: 67.09%] - LeetPush

--- 8-String-to-Integer-(atoi).cpp
+++ 8-String-to-Integer-(atoi).cpp
@@ -3,12 +3,15 @@ public:
     int myAtoi(string s) {
         bool spacesIgnored=false, signPassed=false;
         int n=0, sign=1;
-        vector <int> nums;
 
         for (char c : s){
             if ('0'<=c && c<='9'){
                 spacesIgnored=signPassed=true;
-                nums.push_back(c-'0');
+                if ( (long)n*10 + (c-'0') > INT_MAX){
+                    n = sign==1 ? INT_MAX : INT_MIN;
+                    return n;
+                }
+                else n = n*10 + (c-'0');
             }
             else if (c=='-' && !signPassed) {
                 spacesIgnored= signPassed= true;
@@ -19,15 +22,6 @@ public:
             }
             else if (c==' ' && !spacesIgnored) continue;
             else break;
-        }
-
-        for (int i=nums.size()-1; i>=0; i--)
-        {
-            if (n+nums[i]*pow(10,nums.size()-1-i)>INT_MAX){
-                n = sign==1 ? INT_MAX : INT_MIN;
-                return n;
-            }
-            else n += nums[i]*pow(10,nums.size()-1-i);
         }
         
         return n*sign;
