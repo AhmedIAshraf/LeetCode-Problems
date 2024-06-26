82d64ee74972855c921c0044b46c4af2388157fa
Ahmed Ashraf <121035001+AhmedIAshraf@users.noreply.github.com>
2024-06-26 18:17:28 +0300

    [538] [Time Beats: 25.54%] [Memory Beats: 38.46%] - LeetPush

---
+++ LeetCode/538-Convert-BST-to-Greater-Tree.cpp
@@ -0,0 +1,32 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    TreeNode* convertBST(TreeNode* root) {
+        if (root != NULL )accumulate(root, 0);
+        return root;
+    }
+
+    int accumulate(TreeNode* n, int sum){
+
+        if (n->right != NULL) 
+            sum = accumulate(n->right, sum);
+        
+        n->val += sum;
+        sum = n->val ;
+
+        if (n->left != NULL) 
+            sum = accumulate(n->left, sum);
+
+        return sum;
+    }
+};
