8bcadaaaa70c21996ab27488cb86518345265cc8
Ahmed Ashraf <121035001+AhmedIAshraf@users.noreply.github.com>
2024-07-31 02:33:36 +0300

    102-C

---
+++ CodeForces/Help Vasilisa the Wise 2.cpp
@@ -0,0 +1,66 @@
+#include <iostream>
+#include <bits/stdc++.h>
+
+#define PI 3.14159265359
+#define ull unsigned long long
+#define ll long long
+#define ascSort(v) sort(v.begin(), v.end())
+#define desSort(v) sort(v.rbegin(), v.rend())
+using namespace std;
+
+vector<ull> factorial(21, 1);
+
+ull c(int n, int r)
+{
+    int q = n - r;
+    ull ans = 1;
+    if (n <= 20)
+        return (factorial[n] / factorial[r] / factorial[q]);
+
+    for (int i = n; i > max(r, q); i--)
+        ans *= i;
+    return (ans / factorial[min(r, q)]);
+}
+
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    int r1, r2, c1, c2, d1, d2;
+    cin >> r1 >> r2 >> c1 >> c2 >> d1 >> d2;
+    int b = 0, c = 0, d = 0;
+    vector<bool> done(10, 0);
+
+    for (int a = 1; a < r1 && a < c1 && a < d1; a++)
+    {
+        done = vector<bool>(10, 0);
+        done[a] = 1;
+
+        b = r1 - a;
+        if (done[b])
+            continue;
+        done[b] = 1;
+
+        c = c1 - a;
+        if (done[c])
+            continue;
+        done[c] = 1;
+
+        d = d1 - a;
+        if (done[d])
+            continue;
+        done[d] = 1;
+
+        if (a * b * c * d > 0 && a <= 9 && b <= 9 && c <= 9 && d <= 9)
+        {
+            if (c + d == r2 && b + d == c2 && b + c == d2)
+            {
+                cout << a << " " << b << endl
+                     << c << " " << d;
+                return 0;
+            }
+        }
+    }
+    cout << -1;
+}
