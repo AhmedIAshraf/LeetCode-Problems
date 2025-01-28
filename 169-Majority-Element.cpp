c0f28b2ec3c4d5350edd2674e7a7b991f78559e5
Ahmed Ashraf <121035001+AhmedIAshraf@users.noreply.github.com>
2025-01-28 23:49:47 +0200

    [169] [Time Beats: 7.19%] [Memory Beats: 99.92%] - LeetPush

--- 169-Majority-Element.cpp
+++ 169-Majority-Element.cpp
@@ -1,8 +1,19 @@
 class Solution {
 public:
+
+    pair<int, int> recurse (vector<int>& v, int l, int r){
+        if (l==r) return {v[l], 1};
+        int m = (r+l)/2; 
+        pair<int, int> left, right;
+        left = recurse (v, l, m);
+        right = recurse (v, m+1, r);
+        if (left.first == right.first) return {left.first, left.second+right.second};
+        if (left.second > right.second) return left;
+        else if (left.second < right.second) return right;
+        else return {left.first, 0};
+    }
+
     int majorityElement(vector<int>& nums) {
-        sort(nums.begin(), nums.end());
-        int n = nums.size();
-        return nums[n/2];
+        return recurse(nums, 0, nums.size()-1).first;
     }
 };
