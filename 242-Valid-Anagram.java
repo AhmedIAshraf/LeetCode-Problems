fc91787fbe870720ff50e9041e18a6af317caa5b
Ahmed Ashraf <121035001+AhmedIAshraf@users.noreply.github.com>
2025-02-01 01:34:15 +0200

    [242] [Time Beats: 53.90%] [Memory Beats: 94.69%] - LeetPush

---
+++ 242-Valid-Anagram.java
@@ -0,0 +1,16 @@
+class Solution {
+    public boolean isAnagram(String s, String t) {
+       if (s.length() != t.length())
+            return false;
+       int chars[] = new int[26];
+       for (int i=0; i<s.length(); i++){
+            chars[s.charAt(i)-'a']++;
+            chars[t.charAt(i)-'a']--;
+       }
+       for (int i=0; i<26; i++){
+            if (chars[i] != 0)
+                return false;
+       }
+       return true;
+    }
+}
