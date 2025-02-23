dac40b1e37c6f1aa73792e359eb7f2d1d79a6a50
Ahmed Ashraf <121035001+AhmedIAshraf@users.noreply.github.com>
2025-02-23 21:00:01 +0200

    [2706] [Time Beats: 68.67%] [Memory Beats: 19.59%] - LeetPush

---
+++ 2706-Buy-Two-Chocolates.java
@@ -0,0 +1,9 @@
+class Solution {
+    public int buyChoco(int[] prices, int money) {
+        
+        Arrays.sort(prices);
+        int cost = prices[0] + prices[1];
+
+        return money >= cost ? money-cost : money;
+    }
+}
